6509b9c8961051f7293345ae52570396a2bb4d30
ngoyal <ngoyal@node0.finalproject.eel6892-sp24-pg0.utah.cloudlab.us>
2024-04-13 14:41:37 -0600

    add install steps for gvisor(runsc) runtime)

---
+++ src/gvisor/install.sh
@@ -0,0 +1,19 @@
+# https://gvisor.dev/docs/user_guide/install/
+#!bin/bash
+(
+  set -e
+  ARCH=$(uname -m)
+  URL=https://storage.googleapis.com/gvisor/releases/release/latest/${ARCH}
+  wget ${URL}/runsc ${URL}/runsc.sha512 \
+    ${URL}/containerd-shim-runsc-v1 ${URL}/containerd-shim-runsc-v1.sha512
+  sha512sum -c runsc.sha512 \
+    -c containerd-shim-runsc-v1.sha512
+  rm -f *.sha512
+  chmod a+rx runsc containerd-shim-runsc-v1
+  sudo mv runsc containerd-shim-runsc-v1 /usr/local/bin
+)
+
+# Post installation steps
+# /usr/local/bin/runsc install
+# sudo systemctl reload docker
+# docker run --rm --runtime=runsc hello-world
